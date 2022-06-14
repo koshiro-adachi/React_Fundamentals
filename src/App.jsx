2209db68f7aed5a355779f00a3236554f1f375fa
koshiro-adachi <107029087+koshiro-adachi@users.noreply.github.com>
2022-06-14 18:36:21 +0900

    react_Fundamentals(2)20220614 add props,state(useState,useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+/*eslint react-hooks/exhaustive-deps:off*/
+import React, { useEffect, useState } from "react";
 //ReactDomはHTMLに反映する時に必要なももので、経由させるだけの今回のファイルには必要ない
 import ColorfullMessage from "./compornents/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert("yeahhhhhhh");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickCountReset = () => {
+    setNum(0);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfullMessage color="blue">元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">元気です！</ColorfullMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickCountReset}>カウントリセット</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(._.)</p>}
     </>
   );
 };
